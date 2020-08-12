c0392cbd07b61316d563ae9b7c40cebd5a748c2b
rabbin <769386352@qq.com>
2020-08-12 22:05:40 +0800

    建造者模式

---
+++ src/main/java/com/rabbin/funtionalprogramming/gof/com/rabbin/funtionalprogramming/gof/Builder.java
@@ -0,0 +1,29 @@
+package com.rabbin.funtionalprogramming.gof.com.rabbin.funtionalprogramming.gof;
+
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+
+public class Builder {
+    public static void main(String[] args) {
+        Supplier<Person> getPerson = Person::new;
+        Person person = getPerson.get();
+        System.out.println(person.getName());
+
+        BiConsumer<Person, String> changePersonName = Person::setName;
+        changePersonName.accept(person, "rabbin");
+
+        System.out.println(person.getName());
+    }
+}
+
+class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
